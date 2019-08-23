ae325976b762a2148e31b7fff509c351c7198824
deethompson <devon.thompson0@gmail.com>
2019-08-23 14:46:04 -0400

    Bullhorn part 1

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+
+@Entity
+public class Message {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=4)
+    private String content;
+
+    @NotNull
+    @Size (min=3)
+    private String date;
+
+    @NotNull
+    @Size(min=3)
+    private String sent;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSent() {
+        return sent;
+    }
+
+    public void setSent(String sent) {
+        this.sent = sent;
+    }
+}
